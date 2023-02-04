570fd5d98b69ac0fd7f8be8ff2e79073444e78d1
Satveer Ganasan <2785448G@student.gla.ac.uk>
2023-02-04 15:35:26 -0800

    8th chapter commit

--- rango/views.py
+++ rango/views.py
@@ -17,6 +17,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    print(request.method)
+    print(request.user)
     context_dict = {'boldmessage': 'This tutorial has been put together by Satveer'}
     return render(request, 'rango/about.html', context=context_dict)
 
